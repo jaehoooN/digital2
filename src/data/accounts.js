8e0ca5a112b06f92bea18014e2a594eb1a38fef8
jaehoooN <41664357+jaehoooN@users.noreply.github.com>
2025-07-07 11:22:02 +0900

    Update accounts.js

--- src/data/accounts.js
+++ src/data/accounts.js
@@ -1,12 +1,64 @@
+// src/data/accounts.js
+
 export default [
-  { name: "류종득", title: "디지털본부장", route: "rjd", link: "https://example.com/rjd" },
-  { name: "백상현", title: "디지털계획처장", route: "bsh", link: "https://example.com/bsh" },
-  { name: "최우진", title: "디지털계획부장", route: "cwj", link: "https://example.com/cwj" },
-  { name: "권봉경", title: "첨단융복합부장", route: "kbk", link: "https://example.com/kbk" },
-  { name: "손동윤", title: "AI데이터부장", route: "sdy", link: "https://example.com/sdy" },
-  { name: "송권수", title: "MaaS사업부장", route: "sgs", link: "https://example.com/sgs" },
-  { name: "이강덕", title: "디지털관리처장", route: "lgd", link: "https://example.com/lgd" },
-  { name: "최재억", title: "디지털관리부장", route: "cje", link: "https://example.com/cje" },
-  { name: "서오주", title: "경영시스템부장", route: "soj", link: "https://example.com/soj" },
-  { name: "김용진", title: "기술시스템부장", route: "kyj", link: "https://example.com/kyj" }
+  {
+    name: "백상현",
+    title: "디지털계획처장",
+    route: "paek",
+    link: "po-coedit://mobile.docs.polarisoffice.com?company=a32a816f4bd70480295961b345b7b99e&docsID=313d292c75552a411d5ddf72db35b96d&docsPass=45c886e3b373eb9fc82b1f699f6e399b3c5acb31e7a19a6ecf2bc9e0a3ddac20"
+  },
+  {
+    name: "최우진",
+    title: "디지털계획부장",
+    route: "cwj71",
+    link: "po-coedit://mobile.docs.polarisoffice.com?company=a32a816f4bd70480295961b345b7b99e&docsID=e46b83573d8f01d290b6c3fcb50ceef7&docsPass=577c98fc5a4e51b2cd08b7e9a70c7475733ab376de8206c886fc8c1cb12a823e"
+  },
+  {
+    name: "권봉경",
+    title: "첨단융복합부장",
+    route: "bk_kwon",
+    link: "po-coedit://mobile.docs.polarisoffice.com?company=a32a816f4bd70480295961b345b7b99e&docsID=81c7e263e9733eb4bc53cfb07bfa5ce4&docsPass=c38d6594dc27df4d1cc2b81e35c84746b3c44664095dc3fa07078a3f02d25b82"
+  },
+  {
+    name: "손동윤",
+    title: "AI데이터부장",
+    route: "mrson",
+    link: "po-coedit://mobile.docs.polarisoffice.com?company=a32a816f4bd70480295961b345b7b99e&docsID=861b719a71e8585312ef8e27e24e37f3&docsPass=3fe991b518b60e5dbe6c4264f17cfe2d1f6f83c1f30f7cd10c6f8fd87a651169"
+  },
+  {
+    name: "송권수",
+    title: "MaaS사업부장",
+    route: "sks0919",
+    link: "po-coedit://mobile.docs.polarisoffice.com?company=a32a816f4bd70480295961b345b7b99e&docsID=2e679c59bdba823b75cfa6a8f09c0e14&docsPass=84b69065fe13ad3d4f4cc85a321f337c0a5c79823d23a68f94a36ad4483cf949"
+  },
+  {
+    name: "이강덕",
+    title: "디지털관리처장",
+    route: "kdlee",
+    link: "po-coedit://mobile.docs.polarisoffice.com?company=a32a816f4bd70480295961b345b7b99e&docsID=cb14eeb11a9e365255ca9be381ff0b49&docsPass=7a75aa2be11cd3798b7c2e4602df25c3519c395e1c0abfdac76cf1a36dc649ae"
+  },
+  {
+    name: "최재억",
+    title: "디지털관리부장",
+    route: "chjwuk",
+    link: "po-coedit://mobile.docs.polarisoffice.com?company=a32a816f4bd70480295961b345b7b99e&docsID=16f52513bd15c840e086dd748e109f91&docsPass=29240463e779b94f9e01e295f8d0be76f26c460cf8f17020fdc169f1ef12a86c"
+  },
+  {
+    name: "서오주",
+    title: "경영시스템부장",
+    route: "wizard",
+    link: "po-coedit://mobile.docs.polarisoffice.com?company=a32a816f4bd70480295961b345b7b99e&docsID=67c899745d6f009a2db18471db0fcd99&docsPass=22b7cb64018589c6cfd50bcb13fcb2e3d93f2f01a165d3b19ce7e74c38b234e4"
+  },
+  {
+    name: "김용진",
+    title: "기술시스템부장",
+    route: "comely",
+    link: "po-coedit://mobile.docs.polarisoffice.com?company=a32a816f4bd70480295961b345b7b99e&docsID=3d1611fd77bfa8cd0144b2456d56c938&docsPass=0ad958c9f7352b6d9de3990f282c49c7a274f8cf1ab5be5aef5cf5a13a2e18c7"
+  },
+  {
+    name: "류종득",
+    title: "디지털본부장",
+    route: "ryujd",
+    link: "po-coedit://mobile.docs.polarisoffice.com?company=a32a816f4bd70480295961b345b7b99e&docsID=761ce8936c1b9cef32bda571ffb3cdf3&docsPass=3c0f50eb48227caa88c685223dc5770184f392a0bf432e9354815a6a923d2054"
+  }
 ];
